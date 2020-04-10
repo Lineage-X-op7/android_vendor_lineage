9567b33fda2e698045ac785a825f93f83e9c49c6
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-04-10 20:48:48 +0200

    [2/5] Port face unlock feature
    Build face unlock for arm64 devices
    
    Also add a flag to opt-out
    
    Change-Id: I3bcb011e6b6c20dc076776828090bef0fc738b3a

--- config/common.mk
+++ config/common.mk
@@ -116,6 +116,18 @@ PRODUCT_PACKAGES += \
     LineageSetupWizard \
     Updater
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifeq ($(TARGET_USES_FACELOCK),true)
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Themes
 PRODUCT_PACKAGES += \
     LineageThemesStub \
